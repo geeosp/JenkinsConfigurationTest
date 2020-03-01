5c5798beb7b8b1953389a900042b87cab092eebc
Geovane Pereira <geeo.sp@gmail.com>
2020-03-01 15:31:02 -0300

    Add rotating cube

---
+++ Assets/RotateForever.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateForever : MonoBehaviour
+{
+    public Vector3 velocity = Vector3.up;
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(velocity);
+    }
+}
